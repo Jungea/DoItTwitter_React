35555a5d8d70f1fc28f5086ec567f5a90105f11d
jea <jea@onlinepowers.com>
2023-03-10 18:02:35 +0900

    네이게이션 컴포넌트 생성

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,19 +1,21 @@
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Navigation from "components/Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
-  return (
-    <Router>
-      <Routes>
-        {isLoggedIn ? (
-          <Route exact path="/" element={<Home />} />
-        ) : (
-          <Route exact path="/" element={<Auth />} />
-        )}
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            {isLoggedIn && <Navigation />}
+            <Routes>
+                {isLoggedIn ? (
+                    <Route exact path="/" element={<Home />} />
+                ) : (
+                    <Route exact path="/" element={<Auth />} />
+                )}
+            </Routes>
+        </Router>
+    );
 };
 
 export default AppRouter;
